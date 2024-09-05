ff6b2ffc6c450b42b07c4863fe7735e9a582cd9e
Aman Kumar <amancollege04@example.com>
2024-09-05 13:26:39 +0530

    Created first react page

--- basicReactApp/src/App.jsx
+++ basicReactApp/src/App.jsx
@@ -1,6 +1,10 @@
+import First from "./First"
 function App() {
   return (
+    <>
     <h1>Welcome Aman in React</h1>
+    <First/>
+    </>
   )
 }
 
---
+++ basicReactApp/src/first.jsx
@@ -0,0 +1,6 @@
+function First(){
+    return(
+        <h2>Welcome to your first react code</h2>
+    )
+}
+export default First
