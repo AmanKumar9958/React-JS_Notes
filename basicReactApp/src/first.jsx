77322c2a5da343571f5d9883a8d028cdf0570718
Aman Kumar <amancollege04@example.com>
2024-09-16 06:55:07 +0530

    Created a basic react and vite-react app and learned how to clean the code.

--- basicReactApp/src/first.jsx
+++
@@ -1,6 +0,0 @@
-function First(){
-    return(
-        <h2>Welcome to your first react code</h2>
-    )
-}
-export default First
---
+++ basics/my-react-app/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello 👋 Aman Kumar, welcome to your first React App</h1>
+  );
+}
+
+export default App;
