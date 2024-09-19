41a82eb53961aa0d2e3bfd044763950a5868dc7c
Aman Kumar <amancollege04@example.com>
2024-09-19 12:34:04 +0530

    Learned to create elements..

--- basics/my-vite-app/src/App.jsx
+++ basics/my-vite-app/src/App.jsx
@@ -1,9 +1,11 @@
 // calling custom
 import Aman from "./aman.jsx"
 function App() {
+  const user = 'AmanWebDev24'
   return (
     <>
       <Aman/>
+      <h2>Calling user: {user}</h2>
       {/* <h1>Hello 👋 Aman Kumar, welcome to your first Vite React Project.</h1> */}
       <h3>Chai or Code</h3>
     </>
--- basics/my-vite-app/src/main.jsx
+++ basics/my-vite-app/src/main.jsx
@@ -1,9 +1,43 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+// creating and calling in the same file..
+function MyApp(){
+  return(
+    <div>
+      <h2>My Custom React App..</h2>
+    </div>
+  )
+}
+// 1st way..
+const ReactElement = {
+  type: 'a',  // Tag
+  props:{     // Properties
+      href: 'https://google.com',
+      target: '_blank'
+  },
+  children: 'Click me to visit google'    // Text to be displayed on webpage
+}
+
+// 2nd way..
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Click here to visit</a>
 )
+
+// 3rd way..
+const anotherReactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: "_blank"},
+  'Please click me to visit google..'
+)
+createRoot(document.getElementById('root')).render(
+    <App />
+    // <MyApp/>
+    // ReactElement
+    // anotherElement
+    // anotherReactElement
+)
+
+
