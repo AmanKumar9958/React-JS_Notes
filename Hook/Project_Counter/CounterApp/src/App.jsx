310c0f519a03ae739c5dac6171dd67892c9db574
Aman Kumar <amancollege04@example.com>
2024-09-20 07:13:08 +0530

    Added some extra functionality in the counter App..

--- Hook/Project_Counter/CounterApp/src/App.jsx
+++ Hook/Project_Counter/CounterApp/src/App.jsx
@@ -1,19 +1,32 @@
+import React from 'react'
 import { useState } from 'react'
-
 function App() {
-  const userName = 'Aman Kumar'
-  let counter = 15
+  let[counter, setCounter] = useState(0);  // counter -> variable name, setCounter -> function that will control, useState() -> any default value or any empty string.
 
+  const userName = 'Aman Kumar'
+
+
+  // to use this counter in any other place use the variable 'counter'..
+
+  // increasing value..
   const addValue = () => {
-    counter += 1
+    if(counter < 20){
+      setCounter(counter + 1)
+    }
+  }
+  // decreasing value..
+  const removeValue = () => {
+    if(counter > 0){
+      setCounter(counter - 1)
+    }
   }
   return (
     <>
-    <h1>Hello {userName}, welcome to the CounterApp Project!!</h1>
-    <h1>Counter Value: {counter}</h1>
-    <button onClick={addValue}>Add Value</button>
-    <br />
-    <button onClick={removeValue}>Remove Value</button>
+      <h1>Hello {userName}, welcome to the CounterApp Project!!</h1>
+      <h1>Counter Value: {counter}</h1>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
     </>
   )
 }
